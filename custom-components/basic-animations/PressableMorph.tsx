da79cd1be9cbef7534c4eae59e1e63d3c507ed27
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-06-14 16:43:49 +0800

    Update PressableMorph.tsx

--- custom-components/basic-animations/PressableMorph.tsx
+++ custom-components/basic-animations/PressableMorph.tsx
@@ -5,6 +5,17 @@ const PressableMorph = styled(AnimatedPressable, {
     w:"$20",
     h:"$20",
     backgroundColor:"$rose500",
+    left: 150,
+    // ":initial": {
+    //     "rotate": 0,
+
+    // },
+    // ":animate": {
+    //     "rotate": 200,
+    // },
+    // ":onAnimationComplete": {
+    //     x: 100,
+    // }
 }); 
 
 export default PressableMorph;
