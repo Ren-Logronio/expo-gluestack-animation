4cf9450a183d013531d9eaa67d3dc455ee107ba2
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-06-14 16:43:46 +0800

    created generic animated pressable

---
+++ custom-components/AnimatedPressable.tsx
@@ -0,0 +1,8 @@
+import { AnimatedPressable } from "@gluestack-style/animation-resolver";
+import { styled } from "@gluestack-style/react";
+
+const CustomAnimatedPressable = styled(AnimatedPressable, {
+    h: "$16", w: "$16", backgroundColor: "$rose500"
+})
+
+export default CustomAnimatedPressable;
