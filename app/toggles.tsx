36e2cde9c7e6adcd7eb0699d9ba17a2a98661d92
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-06-14 16:41:48 +0800

    added whiletap, stiffness, and dampening animation to toggles page

--- app/toggles.tsx
+++ app/toggles.tsx
@@ -6,6 +6,17 @@ import {
     HStack,
     Heading,
     ScrollView,
+    SelectBackdrop,
+    SelectIcon,
+    SelectInput,
+    Slider,
+    SliderFilledTrack,
+    SliderThumb,
+    SliderTrack,
+    SelectDragIndicatorWrapper,
+    SelectItem,
+    SelectDragIndicator,
+    SelectContent,
     Text,
     VStack,
     styled,
@@ -14,45 +25,144 @@ import {
   import { Link } from "expo-router";
   import { AnimatePresence, AnimatedText, AnimatedView } from "@gluestack-style/animation-resolver";
   import { useState } from "react";
-  import { AnimatedHeader } from "@/custom-components/AnimatedHeader";
-import LeftAndRight from "@/custom-components/basic-animations/LeftAndRight";
 import Tap from "@/custom-components/toggle-animations/Tap";
+import CustomVStack from "@/custom-components/CustomVStack";
+import AnimatedBoxView from "@/custom-components/AnimatedBoxView";
+import { ChevronDown } from "lucide-react-native";
+import { Select } from "@gluestack-ui/themed";
+import { SelectTrigger } from "@gluestack-ui/themed";
+import { Icon } from "@gluestack-ui/themed";
+import { SelectPortal } from "@gluestack-ui/themed";
+import Filler from "@/custom-components/Filler";
+import CustomAnimatedPressable from "@/custom-components/AnimatedPressable";
+
+
   
-  // const StyledAnimatedView = styled(AnimatedView, {
-  //   ":initial": {
-  //     backgroundColor: "$amber100",
-  //     scale: 0,
-  //     marginTop: 0,
-  //   },
-  // });
-  
-  // const StyledText = styled(Text, {
-  //   backgroundColor: "$transparent",
-  //   color: "$black",
-  // });
-  
-  export default function Root() {
-    const [toggle, setToggle] = useState(false);
-  
-    // const StyledAnimatedView = styled(AnimatedView, {
-    //   ":initial": {
-    //     backgroundColor: "$amber100",
-    //     scale: toggle ? 0.5 : 1,
-    //     marginTop: 0,
-    //   },
-    //   ":animate": {
-    //     scale: toggle ? 1 : 0.5,
-    //   }
-    // });
-  
-    return (
-      <VStack flex={1} backgroundColor="$white" space="lg">
-      <Heading>Toggles</Heading>
-      <Tap/>
-      {/* <Center flex={1} h="$full">
-        <Text>Nothing to Show</Text>
-      </Center> */}
-    </VStack>
-    );
-  }
+export default function Root() {
+  const [type, setType] = useState("spring");
+  const [stiffness, setStiffness] = useState(200);
+  const [damping, setDamping] = useState(20);
+  const [duration, setDuration] = useState(250);
+  const [isPressed, setIsPressed] = useState(false);
+  const [isTapped, setIsTapped] = useState(false);
+
+  return (
+    <VStack flex={1} backgroundColor="$white" space="lg">
+    <Heading>Toggles</Heading>
+
+    <Tap/>
+
+    <HStack space="lg">
+
+      <CustomVStack>
+        <Heading>While Tapped</Heading>
+        <CustomAnimatedPressable
+          h="$12"
+          w="$32"
+          initial={{ y: 0 }}
+          animate={{ y: isTapped ? 4 : 0 }}
+          transition={{ y: { type: "spring", stiffness: 1000, dampening: 20, duration: 100, timing:"ms" } }}
+          onPressIn={() => setIsTapped(true)}
+          onPressOut={() => setIsTapped(false)}
+        >
+          <Center flex={1}>
+            <Text color="$white">Hello World</Text>
+          </Center>
+        </CustomAnimatedPressable>
+      </CustomVStack>
+
+    </HStack>
+    
+    
+    <CustomVStack space="lg">
+      <Heading>Stiffness and Dampening</Heading>
+      <Text>Type</Text>
+      <Select onValueChange={(value) => setType(value)} >
+        <SelectTrigger variant="outline" size="md">
+          <SelectInput value={type} placeholder="Select option" />
+          <SelectIcon mr="$3">
+            <Icon as={ChevronDown} />
+          </SelectIcon>
+        </SelectTrigger>
+        <SelectPortal>
+          <SelectBackdrop />
+          <SelectContent>
+            <SelectDragIndicatorWrapper>
+              <SelectDragIndicator />
+            </SelectDragIndicatorWrapper>
+            <SelectItem label="Linear" value="linear" />
+            <SelectItem label="Spring" value="spring" />
+            <SelectItem label="Tween" value="tween" />
+          </SelectContent>
+        </SelectPortal>
+      </Select>
+      <Text>Stiffness ({stiffness})</Text>
+      <Slider
+        defaultValue={stiffness}
+        minValue={1}
+        maxValue={1500}
+        onChange={(value) => setStiffness(value)}
+        size="md"
+        orientation="horizontal"
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <Text>Dampening ({damping})</Text>
+      <Slider
+        defaultValue={damping}
+        minValue={1}
+        maxValue={300}
+        onChange={(value) => setDamping(value)}
+        size="md"
+        orientation="horizontal"
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <Text>Duration ({duration} ms)</Text>
+      <Slider
+        defaultValue={duration}
+        minValue={0}
+        maxValue={3000}
+        onChange={(value) => setDuration(value)}
+        size="md"
+        orientation="horizontal"
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <Button onPress={() => setIsPressed(prev => !prev)}><ButtonText>Animate</ButtonText></Button>
+      <AnimatedBoxView 
+        animate={{
+          x: isPressed ? 200 : 0,
+          backgroundColor: isPressed ? "#00f" : "#0f0",
+        }}
+        transition={{
+          x: {
+            type: type,
+            stiffness: stiffness,
+            damping: damping,
+            duration: duration,
+            timing: "ms",
+          }, 
+          backgroundColor: {
+            type: type,
+            duration: duration,
+            timing: "ms",
+          }
+        }}
+      />
+    </CustomVStack>
+
+    <Filler/>
+  </VStack>
+  );
+}
   
