614f41275eb7c0773863f5a575d6a6e835cce167
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-06-14 16:42:13 +0800

    added most transform, and color animation to index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,85 +1,216 @@
 
 import {
   Box,
-  Button,
-  ButtonText,
-  Center,
   HStack,
   Heading,
-  ScrollView,
   Text,
   VStack,
   styled,
 } from "@gluestack-ui/themed";
-import { View } from "react-native";
-import { Link } from "expo-router";
-import { AnimatePresence, AnimatedText, AnimatedView } from "@gluestack-style/animation-resolver";
 import { useState, useEffect } from "react";
-import AnimationPanel from "@/custom-components/AnimationPanel";
-import LeftAndRight from "@/custom-components/basic-animations/LeftAndRight";
-import LeftAndRightSpring from "@/custom-components/basic-animations/LeftAndRightSpring";
-import LeftAndRightSpringStretch from "@/custom-components/basic-animations/LeftAndRightSpringStretch";
-import LeftRightSpring from "@/custom-components/basic-animations/LeftRightSpring";
-import PressableMorph from "@/custom-components/basic-animations/PressableMorph";
+import { useToggle } from "@/custom-components/ToggleProvider";
+import { AnimatedView } from "@gluestack-style/animation-resolver";
+import CustomVStack from "@/custom-components/CustomVStack";
+import Filler from "@/custom-components/Filler";
+import AnimatedBoxView from "@/custom-components/AnimatedBoxView";
+import { Center } from "@gluestack-ui/themed";
 
+const test = styled(AnimatedView, {
+  
+});
 
-// const StyledAnimatedView = styled(AnimatedView, {
-//   ":initial": {
-//     backgroundColor: "$amber100",
-//     scale: 0,
-//     marginTop: 0,
-//   },
-// });
-
-// const StyledText = styled(Text, {
-//   backgroundColor: "$transparent",
-//   color: "$black",
-// });
 
 export default function Home() {
-  const [toggle, setToggle] = useState(false);
   const [isPressed, setIsPressed] = useState(false);
+  const { autoToggle } = useToggle();
 
-  // const StyledAnimatedView = styled(AnimatedView, {
-  //   ":initial": {
-  //     backgroundColor: "$amber100",
-  //     scale: toggle ? 0.5 : 1,
-  //     marginTop: 0,
-  //   },
-  //   ":animate": {
-  //     scale: toggle ? 1 : 0.5,
-  //   }
-  // });
-  useEffect(() => {
-    console.log("🚀 ~ Home ~ isPressed:", isPressed)
-
-  }, [isPressed]);
-
-  const AnimatedContainer = styled(AnimatedView, {
-
-  });
+  const distance = 200;
 
   return (
     <VStack flex={1} backgroundColor="$white" space="lg">
       <Heading>Basic Animations</Heading>
 
-      {/* <Button onPress={() => setToggle((prevState) => !prevState)}><Text>Show</Text></Button> */}
-      {/* <AnimatePresence> */}
-        <PressableMorph
-            animateProps={{
-              opacity: isPressed ? 0.2 : 1,
+
+      <HStack space="lg">
+
+        <CustomVStack>
+          <Heading>opacity</Heading>
+          <AnimatedBoxView 
+            animate={{
+              opacity: autoToggle ? 0 : 1,
             }}
-            onPress={() => setIsPressed((prevState) => !prevState)}
+            transition={{
+              type: "spring",
+            }}
           />
-      {/* </AnimatePresence> */}
+        </CustomVStack>
 
-        {/* <AnimatePresence>
-        <LeftRightSpring 
-          states={{ pressed: isPressed }}
-          onPress={() => setIsPressed((prevState) => !prevState)}
-        /> 
+        <CustomVStack>
+          <Heading>color</Heading>
+          <AnimatedBoxView 
+            animate={{
+              backgroundColor: autoToggle ? "hsl(23, 100%, 50%)" : "#0bf",
+            }}
+            transition={{
+              type: "spring",
+            }}
+          />
+        </CustomVStack>
 
-        </AnimatePresence> */} 
+        <CustomVStack>
+          <Heading>gradient</Heading>
+          <AnimatedBoxView>
+            <Center flex={1}>
+              <Text color="$white">???</Text>
+            </Center>
+          </AnimatedBoxView>
+        </CustomVStack>
+
+        
+      </HStack>
+
+      <CustomVStack>
+        <Heading>x position</Heading>
+        <AnimatedBoxView 
+          animate={{
+            left: autoToggle ? distance : 0,
+          }} 
+          transition={{
+            type: "spring",
+          }}
+        />
+      </CustomVStack>
+
+      <CustomVStack>
+        <Heading>test</Heading>
+      </CustomVStack>
+
+      <HStack space="lg">
+        
+      <CustomVStack>
+        <Heading>y position</Heading>
+        <AnimatedBoxView 
+          animate={{
+            top: autoToggle ? distance : 0,
+          }} 
+          transition={{
+            type: "spring",
+          }}
+        />
+      </CustomVStack>
+
+        <VStack space="lg">
+          <CustomVStack>
+            <Heading>rotation</Heading>
+            <AnimatedBoxView 
+              animate={{
+                rotate: autoToggle ? "90deg" : "0deg",
+              }}
+              transition={{
+                type: "spring",
+              }}
+            />
+          </CustomVStack>
+
+
+          <CustomVStack>
+            <Heading>rotation x</Heading>
+            <AnimatedBoxView 
+              shadowColor="#000"
+              shadowOpacity={0.2}
+              animate={{
+                rotateX: autoToggle ? "65deg" : "0deg",
+              }}
+              transition={{
+                type: "spring",
+              }}
+            >
+              <Center flex={1}>
+                <Text color="$white">Hello World</Text>
+              </Center>
+            </AnimatedBoxView>
+          </CustomVStack>
+
+
+          <CustomVStack>
+            <Heading>rotation y</Heading>
+            <AnimatedBoxView 
+              animate={{
+                rotateY: autoToggle ? "50deg" : "0deg",
+              }}
+              transition={{
+                type: "spring",
+              }}
+            >
+              <Center flex={1}>
+                <Text color="$white">Hello World</Text>
+              </Center>
+            </AnimatedBoxView>
+          </CustomVStack>
+
+          </VStack>
+        
+      </HStack>
+
+      <CustomVStack>
+        <Heading>rotation xy</Heading>
+        <AnimatedBoxView 
+          animate={{
+            rotateX: autoToggle ? "40deg" : "0deg",
+            rotateY: autoToggle ? "40deg" : "0deg",
+          }}
+          transition={{
+            type: "spring",
+          }}
+        >
+          <Center flex={1}>
+            <Text color="$white">Hello World</Text>
+          </Center>
+        </AnimatedBoxView>
+      </CustomVStack>
+
+      <HStack space="md">
+
+        <CustomVStack>
+          <Heading>scale</Heading>
+          <AnimatedBoxView 
+            animate={{
+              scale: autoToggle ? 0.5 : 1,
+            }}
+            transition={{
+              type: "spring",
+            }}
+          />
+        </CustomVStack>
+
+        <CustomVStack>
+          <Heading>scale x</Heading>
+          <AnimatedBoxView 
+            animate={{
+              scaleX: autoToggle ? 0.5 : 1,
+            }}
+            transition={{
+              type: "spring",
+            }}
+          />
+        </CustomVStack>
+
+        <CustomVStack>
+          <Heading>scale y</Heading>
+          <AnimatedBoxView 
+            animate={{
+              scaleY: autoToggle ? 0.5 : 1,
+            }}
+            transition={{
+              type: "spring",
+            }}
+          />
+        </CustomVStack>
+
+      </HStack>
+      
+
+      <Filler />
     </VStack>
   );
 }
