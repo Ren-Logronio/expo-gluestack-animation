51069cc65d7f2524b5b821095f745ad561fd1b02
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-06-14 16:43:33 +0800

    created generic animated box component

---
+++ custom-components/AnimatedBoxView.tsx
@@ -0,0 +1,8 @@
+import { AnimatedView } from "@gluestack-style/animation-resolver";
+import { styled } from "@gluestack-style/react";
+
+const AnimatedBoxView = styled(AnimatedView, {
+    h: "$16", w: "$16", backgroundColor: "$rose500"
+  });
+
+export default AnimatedBoxView;
